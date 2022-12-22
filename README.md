5e34216f6b0f5fc8da783289cd1f8c4ca4aec155
rfl890 <rfl890mc@gmail.com>
2022-12-22 18:11:08 -0500

    the

---
+++ README.md
@@ -0,0 +1,48 @@
+# base91-js   
+[basE91](https://base91.sourceforge.net/) encoding for the browser. Ported from the base91 PHP code.   
+
+## Usage
+## Browser
+Include this script tag in your `<head>`:
+```html
+<script src="https://cdn.jsdelivr.net/npm/base91-js@1.0.3/dist/base91.min.js"></script>
+```
+## Node, AMD (RequireJS)
+```js
+const {
+    encode,
+    decode,
+    create
+} = require("base91-js");
+/*
+or
+const base91 = require("base91-js");
+*/
+```
+### To encode:
+```javascript
+const textToEncode = "Hello 👋. 这是一些中文文本. А вот и русский текст."
+const encoded = base91.encode(textToEncode);
+console.log(encoded);
+```
+### To decode:
+```javascript
+const textToDecode = ">OwJh>&\"zkMICB;`TkMK)9C+cLmp_Zv=k2k`F[fVYUifCB2k/t7fj/^xtLg4FE+k4YER7tKFf#+4FE9gIY)F`#n;`od56[C"
+const decoded = base91.decode(textToDecode);
+console.log(decoded);
+```
+### Chunked encoding   
+Note: Chunked encoding uses typed arrays, not strings, so you'll need to use a `TextEncoder` if you want to use strings.
+```javascript
+const encoder = new TextEncoder();
+const base91_encoder = base91.create();
+const view1 = encoder.encode("Hello, ");
+const view2 = encoder.encode("world");
+
+base91_encoder.update(view1);
+base91_encoder.update(view2);
+const result = base91_encoder.finalize();
+```
+
+# Credits   
+Joachim Henke - basE91 library and source code author
---
+++ src/base91.js
@@ -0,0 +1,140 @@
+(function () {
+    const encodingChars =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,./:;<=>?@[]^_`{|}~"';
+
+    function base91_encode(input) {
+        const encoder = new TextEncoder();
+        const bytes = encoder.encode(input);
+        const length = bytes.length;
+        let result = "";
+        let n = 0;
+        let b = 0;
+        for (let i = 0; i < length; i++) {
+            b |= bytes[i] << n;
+            n += 8;
+            if (n > 13) {
+                let v = b & 8191;
+                if (v > 88) {
+                    b >>= 13;
+                    n -= 13;
+                } else {
+                    v = b & 16383;
+                    b >>= 14;
+                    n -= 14;
+                }
+                result +=
+                    encodingChars.charAt(v % 91) +
+                    encodingChars.charAt((v / 91) | 0);
+            }
+        }
+        if (n) {
+            result += encodingChars.charAt(b % 91);
+            if (n > 7 || b > 90) {
+                result += encodingChars.charAt((b / 91) | 0);
+            }
+        }
+        return result;
+    }
+    function base91_decode(input) {
+        const length = input.length;
+        const decoder = new TextDecoder();
+        let result = [];
+        let b = 0;
+        let n = 0;
+        let v = -1;
+        for (let i = 0; i < length; i++) {
+            const c = encodingChars.indexOf(input[i]);
+            if (c === -1) continue;
+            if (v < 0) {
+                v = c;
+            } else {
+                v += c * 91;
+                b |= v << n;
+                n += (v & 8191) > 88 ? 13 : 14;
+                do {
+                    result.push(b & 0xff);
+                    b >>= 8;
+                    n -= 8;
+                } while (n > 7);
+                v = -1;
+            }
+        }
+        if (v > -1) {
+            result.push((b | (v << n)) & 0xff);
+        }
+        const view = new Uint8Array(result);
+        const decoded = decoder.decode(view);
+        return decoded;
+    }
+
+    function base91_createEncode() {
+        const decoder = new TextDecoder();
+        let b = 0;
+        let n = 0;
+        let result = "";
+        return {
+            update: function (chunk) {
+                for (let i = 0; i < chunk.length; i++) {
+                    b |= chunk[i] << n;
+                    n += 8;
+                    if (n > 13) {
+                        let v = b & 8191;
+                        if (v > 88) {
+                            b >>= 13;
+                            n -= 13;
+                        } else {
+                            v = b & 16383;
+                            b >>= 14;
+                            n -= 14;
+                        }
+                        result +=
+                            encodingChars.charAt(v % 91) +
+                            encodingChars.charAt((v / 91) | 0);
+                    }
+                }
+            },
+            finalize: function () {
+                if (!this.finalized) {
+                    if (n) {
+                        result += encodingChars.charAt(b % 91);
+                        if (n > 7 || b > 90) {
+                            result += encodingChars.charAt((b / 91) | 0);
+                        }
+                    }
+                    this.finalized = true;
+                    return result;
+                } else {
+                    return result;
+                }
+            },
+            finalized: false,
+        };
+    }
+    if (typeof module !== "undefined" && module.exports) {
+        // CommonJS
+        module.exports = {
+            encode: base91_encode,
+            decode: base91_decode,
+            create: base91_createEncode,
+        };
+        return;
+    } else if (typeof define === "function" && define.amd) {
+        // AMD
+        define(["base91-js"], function () {
+            return {
+                encode: base91_encode,
+                decode: base91_decode,
+                create: base91_createEncode,
+            };
+        });
+        return;
+    } else if (typeof window !== "undefined") {
+        // Script tag
+        window.base91 = {
+            encode: base91_encode,
+            decode: base91_decode,
+            create: base91_createEncode,
+        };
+        return;
+    }
+})();
